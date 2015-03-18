e5876973ce865746aa71824a24489afaf7187fef
Lodewicus Adriaanse <lodewicus@gmail.com>
2015-03-18 09:59:14 +0200

    minor changes to test

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -1,6 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe StaticPagesController, type: :controller do
+feature "Visiting the 'Home' page" do
+	before do
+		visit root_path
+	end
 
-
+	scenario "Contact button should work and redirect to contact page" do
+  	click_link('Contact', match: :first)
+		expect(page).to have_content('Call: 082-875-6591')	
+	end
 end
