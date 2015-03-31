da6d9befe3d5f0544991f6bd1ab2ba0977f605a3
Lodewicus Adriaanse <lodewicus@gmail.com>
2015-03-31 15:02:30 +0200

    Contact details changed

--- app/models/bio.rb
+++ app/models/bio.rb
@@ -1,6 +1,6 @@
 class Bio < ActiveRecord::Base
 	mount_uploader :profile_pic, PicturesUploader
-	validates :name, :info, :body, :profile_pic, presence: true
+	validates :name, :info, :body, presence: true
 	validates :name, :info, length: { maximum: 25 }
 
 
