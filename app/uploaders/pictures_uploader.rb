1409a9eed33872602860627ebca18b71ce63ab74
Lodewicus Adriaanse <lodewicus@gmail.com>
2015-04-14 14:26:14 +0200

    changes to images display

--- app/uploaders/pictures_uploader.rb
+++ app/uploaders/pictures_uploader.rb
@@ -40,6 +40,10 @@ class PicturesUploader < CarrierWave::Uploader::Base
     process :resize_to_fit => [800, 800]
   end
 
+  version :mobile do
+    process :resize_to_fit => [533, 533]
+  end
+
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   # def extension_white_list
